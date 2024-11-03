5299473894678b3c1c94f48fb70f03a6aecaa951
Filipe Pina <hzlu1ot0@duck.com>
2024-11-03 23:53:36 +0000

    x

---
+++ tests/test_entrypoint.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import unittest
+from unittest import mock
+import entrypoint
+import tempfile
+
+
+# TODO: do not forget to add testcases that make sense to you
+# Good coverage will make it much easier/safer to just merge those dependabot PRs
+class Test(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        # backup environment
+        self._env = {**os.environ}
+        self.out_file = Path(tempfile.NamedTemporaryFile(delete=False).name)
+    
+    def tearDown(self) -> None:
+        os.environ = self._env
+        self.out_file.unlink()
+
+    def test_sum(self):
+        os.environ.update({
+            'GITHUB_OUTPUT': str(self.out_file),
+            'INPUT_NUMBER-ONE': '1',
+            'INPUT_NUMBER-TWO': '10',
+        })
+        entrypoint.main()
+        output = self.out_file.read_text().strip()
+        self.assertEqual(output, 'sum=11')
+    
+    @mock.patch('entrypoint.requests')
+    def test_joke(self, req_mock):
+        req_mock.get.return_value.json.return_value = {'joke': 'haha'}
+        os.environ.update({
+            'GITHUB_OUTPUT': str(self.out_file),
+        })
+        entrypoint.main()
+        output = self.out_file.read_text().strip()
+        self.assertEqual(output, 'sum=haha')
