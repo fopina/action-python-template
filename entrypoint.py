e68963f3a3dd251a99341d59b6098ac0f15f995e
Filipe Pina <hzlu1ot0@duck.com>
2024-11-03 23:07:30 +0000

    x

--- entrypoint.py
+++ entrypoint.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env -S python3 -B
-
-# NOTE: If you are using an alpine docker image
-# such as pyaction-lite, the -S option above won't
-# work. The above line works fine on other linux distributions
-# such as debian, etc, so the above line will work fine
-# if you use pyaction:4.0.0 or higher as your base docker image.
+#!/usr/bin/env -S python3 -u
 
 import sys
 import os
+import requests
 
 if __name__ == "__main__" :
     # Rename these variables to something meaningful
@@ -21,7 +16,7 @@ if __name__ == "__main__" :
 
     # This is how you produce workflow outputs.
     # Make sure corresponds to output variable names in action.yml
-    if "GITHUB_OUTPUT" in os.environ :
+    if "GITHUB_OUTPUT" in os.environ:
         with open(os.environ["GITHUB_OUTPUT"], "a") as f :
             print("{0}={1}".format("output-one", output1), file=f)
             print("{0}={1}".format("output-two", output2), file=f)
