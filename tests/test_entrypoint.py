4a54e131b6a1c45b6abc621f505292ddf42b211b
Filipe Pina <hzlu1ot0@duck.com>
2024-11-04 12:38:31 +0000

    .x

--- tests/test_entrypoint.py
+++ tests/test_entrypoint.py
@@ -1,9 +1,10 @@
 import os
-from pathlib import Path
-import unittest
-from unittest import mock
-import entrypoint
 import tempfile
+import unittest
+from pathlib import Path
+from unittest import mock
+
+import entrypoint
 
 
 # TODO: do not forget to add testcases that make sense to you
@@ -14,27 +15,31 @@ class Test(unittest.TestCase):
         # backup environment
         self._env = {**os.environ}
         self.out_file = Path(tempfile.NamedTemporaryFile(delete=False).name)
-    
+
     def tearDown(self) -> None:
         os.environ = self._env
         self.out_file.unlink()
 
     def test_sum(self):
-        os.environ.update({
-            'GITHUB_OUTPUT': str(self.out_file),
-            'INPUT_NUMBER-ONE': '1',
-            'INPUT_NUMBER-TWO': '10',
-        })
+        os.environ.update(
+            {
+                'GITHUB_OUTPUT': str(self.out_file),
+                'INPUT_NUMBER-ONE': '1',
+                'INPUT_NUMBER-TWO': '10',
+            }
+        )
         entrypoint.main()
         output = self.out_file.read_text().strip()
         self.assertEqual(output, 'sum=11')
-    
+
     @mock.patch('entrypoint.requests')
     def test_joke(self, req_mock):
         req_mock.get.return_value.json.return_value = {'joke': 'haha'}
-        os.environ.update({
-            'GITHUB_OUTPUT': str(self.out_file),
-        })
+        os.environ.update(
+            {
+                'GITHUB_OUTPUT': str(self.out_file),
+            }
+        )
         entrypoint.main()
         output = self.out_file.read_text().strip()
         self.assertEqual(output, 'sum=haha')
